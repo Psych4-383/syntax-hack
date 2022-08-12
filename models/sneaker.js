adffdd91124ab3d386915fd8457c493ae0e9f56a
mannattuli <mannattuli10@gmail.com>
2022-08-12 11:08:08 +0530

    css

--- models/sneaker.js
+++ models/sneaker.js
@@ -1,16 +1,3 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const sneakerSchema = new Schema({
-    name: String,
-    brand: String,
-    color: String,
-    gender: Boolean,
-    image: String
-});
-
-const Sneaker = mongoose.model('Sneaker', sneakerSchema);
-module.exports = Sneaker;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
